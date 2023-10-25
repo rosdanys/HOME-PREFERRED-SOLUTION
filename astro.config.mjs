a0d2cbd80baef47c5fd889cab840094b52b13416
Ing. Rosdanys Ramirez Arcas <rosdanys@gmail.com>
2023-10-24 23:09:42 -0400

    fix: add verify env

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import compress from 'astro-compress';
 
 import react from "@astrojs/react";
 
+const isProd = import.meta.env.PROD;
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     viewTransitions: true
   },
-  site: "http://localhost:3000",
+  site: isProd? 'https://home-preferred-solution.vercel.app/':"http://localhost:3000",
   integrations: [react(),tailwind(), compress()]
 });
